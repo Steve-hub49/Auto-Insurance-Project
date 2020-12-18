5e7d4a89ac26cb8e0ddd3dcb5910789e9ba3327f
Steve Freeland <stevefreeland1@gmail.com>
2020-12-17 21:30:50 -0500

    added reference_material folder to gitignore statement

--- api/app.py
+++ api/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello World! alsdjalwesjflawejnrc"
+    return "Hello Kourt! alsdjalwesjflawejnrc"
 
 if __name__ == '__main__':
     app.run()
