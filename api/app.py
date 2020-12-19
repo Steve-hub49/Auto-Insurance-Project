0bbcb113886cd5e0e7fdea9afebfb7be3439f3d5
Steve Freeland <stevefreeland1@gmail.com>
2020-12-19 11:40:44 -0500

    another edit to app.py file

--- api/app.py
+++ api/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello all! This is Steve Freeland's flask for Project 2 - It is Saturday"
+    return "Hello all! This is Steve Freeland's flask for Project 2"
 
 if __name__ == '__main__':
     app.run() 
