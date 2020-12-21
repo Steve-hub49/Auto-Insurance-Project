7fc43e794e32222225752c7c1ec4456d218b6d74
Steve Freeland <stevefreeland1@gmail.com>
2020-12-20 19:31:46 -0500

    continued flask api work in visual studio code

--- api/app.py
+++ api/app.py
@@ -1,10 +1,29 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, jsonify
 
+# Dictionary of auto insurance data
+auto_insurance_data = [
+    {"insured_education_level: "Associate", "College", "High School", "JD", "Masters", "MD", "PhD"}
+]
 
-@app.route('/')
-def hello():
-    return "Hello all! This is Steve Freeland's flask for Project 2"
+# Flask Setup
+app = Flask (__name__)
+
+# Flask Routes
+@app.route('/api/auto_insurance/data/QuickDBD-export.sql')
+def insured_education_level():
+    """Return the auto insurance data as json"""
+
+    return jsonify(insured_education_level)
+
+@app.route("/")
+def welcome():
+    return (
+        f"Welcome to the Auto Insurance Data API!<br/>"
+        f"Available Routes:<br/>"
+        f"/PROJECT 2_INSURANCE/auto_insurance/data/QuickDBD-export.sql/insured_education_level"
+    )
+
 
 if __name__ == '__main__':
-    app.run() 
+    app.run(debug=True) 
+    
